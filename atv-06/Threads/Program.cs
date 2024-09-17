57bb3b78e623b254bd202a506176b85c63011e16
Vinicius Tengaten <vinitengaten@gmail.com>
2024-09-17 19:26:06 -0300

    Atv 06 - Threads

---
+++ atv-06/Threads/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Threads;
+
+class Program {
+	static void Main(string[] args) {
+		Thread thread1 = new Thread(new ThreadStart(ImprimirNumeros));
+		Thread thread2 = new Thread(new ThreadStart(ImprimirLetras));
+
+		thread1.Start();
+		thread2.Start();
+
+		thread1.Join();
+		thread2.Join();
+	}
+
+	static void ImprimirNumeros() {
+		for (int i = 0; i < 5; i++) {
+			Console.WriteLine($"Thread 1 - {i + 1}");
+			Thread.Sleep(1000);
+		}
+	}
+
+	static void ImprimirLetras() {
+		for (int i = 0; i < 5; i++) {
+			Console.WriteLine($"Thread 2 - {(char) (65 + i)}");
+			Thread.Sleep(800);
+		}
+	}
+}
+
